c5cd6c43e5eed8d61d00f6157284940bdd21daff
Riccardo Gulin <riccardo.gulin@gmail.com>
2025-07-23 10:54:48 +0200

    Adds Users CRUD

---
+++ src/main/java/riccardogulin/u5d8/controllers/UsersController.java
@@ -0,0 +1,56 @@
+package riccardogulin.u5d8.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import riccardogulin.u5d8.entities.User;
+import riccardogulin.u5d8.payloads.NewUserPayload;
+import riccardogulin.u5d8.services.UsersService;
+
+import java.util.List;
+import java.util.UUID;
+
+/*
+
+1. GET http://localhost:3001/users
+2. POST http://localhost:3001/users (+req.body)
+3. GET http://localhost:3001/users/{userId}
+4. PUT http://localhost:3001/users/{userId} (+req.body)
+5. DELETE http://localhost:3001/users/{userId}
+
+*/
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+	@Autowired
+	private UsersService usersService;
+
+	@GetMapping
+	public List<User> findAll() {
+		return this.usersService.findAll();
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public User save(@RequestBody NewUserPayload payload) {
+		return this.usersService.save(payload);
+	}
+
+	@GetMapping("/{userId}")
+	public User getById(@PathVariable UUID userId) {
+		return this.usersService.findById(userId);
+	}
+
+	@PutMapping("/{userId}")
+	public User getByIdAndUpdate(@PathVariable UUID userId, @RequestBody NewUserPayload payload) {
+		return this.usersService.findByIdAndUpdate(userId, payload);
+	}
+
+	@DeleteMapping("/{userId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void getByIdAndDelete(@PathVariable UUID userId) {
+		this.usersService.findByIdAndDelete(userId);
+	}
+}
